0be30f72ae95d528b5a47805dac6a46dd2fb5cf0
Schrixx <fun_nos@yahoo.com>
2023-07-27 22:57:11 -0700

    refactor: change route pathing

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { Routes, Route } from 'react-router-dom'
 import NotFound from './pages/NotFound'
-import Main from './pages/Main'
+import Artwork from './pages/Artwork'
+import Home from './pages/Home'
 
 const App = () => {
 
   return (
     <>
       <Routes>
-        <Route path='/' element={<Main />} />
+        <Route path='/' element={<Home />} />
         <Route path='*' element={<NotFound />} />
+        <Route path='/artwork' element={<Artwork />} />
       </Routes>
     </>
   )
